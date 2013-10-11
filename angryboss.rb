e9502a93506f65b030ce2819a353d4b58de5df4a
Nick L <nicklonz@gmail.com>
2013-10-11 16:04:53 -0400

    Chapter 5 and 6 Exercises

---
+++ angryboss.rb
@@ -0,0 +1,5 @@
+puts "What is your first name?"
+f_name = gets.chomp
+puts "CAN'T YOU SEE THAT I'M BUSY, #{f_name.upcase}!"
+request = gets.chomp
+puts "WHAT IN THE WORLD DO YOU MEAN \"#{request.upcase}\"!!! YOU'RE FIRED!!!"
---
+++ biggerbetternumber.rb
@@ -0,0 +1,3 @@
+puts "Hello, what is your favorite number?"
+fav_num = gets.chomp.to_i
+puts "That's OK, I guess but isn't #{fav_num +1} just a little bit better?"
